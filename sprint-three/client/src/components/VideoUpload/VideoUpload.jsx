bb047bd859fb1f7483b877278f4106de188055c4
Albert Hackman <nana20p@yahoo.com>
2021-04-17 23:43:55 -0400

    Local API created, Working on Upload Page Functionality

--- sprint-three/client/src/components/VideoUpload/VideoUpload.jsx
+++ sprint-three/client/src/components/VideoUpload/VideoUpload.jsx
@@ -1,37 +1,71 @@
 import React from 'react';
 import img from '../../assets/images/Upload-video-preview.jpg'
-import './videoUpload.scss'
+import './videoUpload.scss';
+import axios from "axios";
+
+
+const expressUrl = "http://localhost:8080";
 
 function VideoUpload(props) {
-
+    // console.log(props)
     //This function takes the user back to the home page with the default video
     //This also gives the user a feedback of a successfull upload
     let handleSubmit = (e) => {
                 e.preventDefault();
+
+                // const submitData = new FormData()
+                // submitData.append("title", e.target.title.value)
+                // submitData.append("description", e.target.description.value)
+                // submitData.append("video", e.target.video.value)
+
+                axios
+                    .post(`${expressUrl}/videos`,{
+                    // url: expressUrl + "videos",
+                    // data: submitData,
+                    // headers: { "Content-Type": "multipart/form-data" },
+                    title: e.target.title.value,
+                    description: e.target.description.value,
+                    video: e.target.video.value,
+                    })
+                    .then(function (response) {
+                        //handle success
+                        console.log(response);
+                    })
+                    .catch(function (response) {
+                        //handle error
+                        console.log(response);
+                });
                 alert("Video has been Uploaded successfully")
-        return  props.history.push("/");
+        // return  props.history.push("/");
     }
 
     
     return (
         <section className="upload">
             <h1 className="upload__title">Upload Video</h1>
+            <form onSubmit={handleSubmit} encType="multipart/form-data">
             <div className="upload__main-wrap">
                 <div className="upload__video-wrap">
                     <label className="upload__thumbnail">VIDEO THUMBNAIL</label>
                     <img src={img} className="upload__video-img" alt="Video Thumbnail"/>
                 </div>
-                <form className="upload__form-wrap">
+                
+                    <div className="upload__form-wrap">
                     <label className="upload__video-label">TITLE YOUR VIDEO</label>
-                    <input className="upload__video-title" type="text" placeholder="Add a title to your video" />
+                    <input name="title" className="upload__video-title" type="text" placeholder="Add a title to your video" />
                     <label className="upload__description-label">ADD A VIDEO DESCRIPTION</label>
-                    <textarea name="input" className="upload__description" placeholder="Add a description of your video"></textarea>
-                </form>
+                    <textarea name="description" className="upload__description" placeholder="Add a description of your video"></textarea>
+                    <label className="upload__video-label" >CHOOSE A VIDEO</label>
+                    <input name="video" className="upload__video-title" type="file" placeholder= "select a video" accept="video/mp4,video/x-m4v,video/*"/>
+                    </div>
+                    
+               
             </div>
             <div className="upload__button-wrap">
-                <button className="upload__button-publish" onClick={handleSubmit}>PUBLISH</button>
+                <button className="upload__button-publish" type="submit">PUBLISH</button>
                 <button className="upload__button-cancel" >CANCEL</button>
             </div>
+             </form>
         </section>
     )
 }
